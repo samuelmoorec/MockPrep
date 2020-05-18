8eb3df731f29ed0cb42b5f8a3d56781143f218e8
Samuel C Moore <48695356+samuelmoorec@users.noreply.github.com>
2020-05-18 17:06:22 -0500

    added question preview to add a question view

--- src/main/resources/static/JS/general.js
+++ src/main/resources/static/JS/general.js
@@ -200,7 +200,59 @@ generatePreHTML = (HTML) => {
     return HTML.replace(/</g ,"&lt").replace(/>/g ,"&gt");
 }
 
-let html = `<p>hello there my bb</p>`
+
+PreHTMLGeneration = () => {
+
+    $(document).on('change', '#question', function() {
+        let question = $(this).val()
+        question = generatePreHTML(question);
+        $('#questionFormatted').val(question)
+
+    });
+
+    $(document).on('change', '#solution', function() {
+        let solution = $(this).val()
+        solution = generatePreHTML(solution);
+        $('#solutionFormatted').val(solution)
+    });
+
+    $(document).on('click','#previewmodal', function () {
+        PreviewModal();
+    });
+
+}
+
+PreviewModal = () => {
+
+    let video = `<div class="video-container">
+                        <iframe width="50%" height="300px" src="https://www.youtube.com/embed/${$('#solution_video').val()}" frameborder="0" allowfullscreen></iframe>
+                     </div>`;
+
+    let question = `<br/><pre>${$('#questionFormatted').val()}</pre>`;
+
+    let solution = `<br/><pre>${$('#solutionFormatted').val()}</pre>`
+
+    let tabs = `<div class="modal-content noScrollBar" style="height: auto" >
+                        <h5 class="center" >${$('#subject').val()}</h5>
+                        <div class="row">
+                            <div class="col s12 ">
+                                <ul class="tabs"  >
+                                  <li class="tab col s4"><a href="#test1" class="active light-green-text text-darken-1">Question</a></li>
+                                  <li class="tab col s4"><a href="#test2" class="light-green-text text-darken-1">Answer</a></li>
+                                  <li class="tab col s4"><a href="#test4" class="light-green-text text-darken-1">Video</a></li>
+                                </ul>
+                            </div>
+                            <div id="test1" class="col s12">${question}</div>
+                            <div id="test2" class="col s12">${solution}</div>
+                            <div id="test4" class="col s12">${video}</div>
+                        </div>
+                    </div>`;
+
+    $('#questionPreview').html(tabs);
+
+    $('#questionPreview').addClass("no-autoinit");
 
 
-console.log(generatePreHTML(html));
+    M.AutoInit();
+}
+
